40d6355f23d02bf3131af00a962d9e3f21f2a23e
Juan Felipe Bustamante <jfbm74@gmail.com>
2025-08-13 21:24:19 -0500

    Add initial setup

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -35,6 +35,7 @@ DJANGO_APPS = [
 
 THIRD_PARTY_APPS = [
     'rest_framework',
+    'rest_framework.authtoken',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
